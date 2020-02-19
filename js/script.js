79db5205eeaa681453ac50c4535de503894e3619
laura <llrstudent03@gmail.com>
2020-02-19 22:07:24 +0000

    adding objects

--- js/script.js
+++ js/script.js
@@ -1,50 +1,87 @@
-let Song =["Hiding"];
+let song1 = {
+    name: "Hiding",
+    artist: "Florence + The Machine",
+    duration: "2:33",
+    link: "https://youtu.be/d7OoIquEdsM"
+};
+let song2 = {
+    name: "Choir",
+    artist: "Florence + The Machine",
+    duration: "2:33",
+    link: "https://www.youtube.com/watch?v=AexrAvyJjJY"
+};
 
-let Artists = ["Florence"];
 
-let Duration = ["2:33"];
 
-let imageAlbum = ["https://images-na.ssl-images-amazon.com/images/I/71tp8YpD02L._SL1428_.jpg"];
 
-let Links = ["https://youtu.be/d7OoIquEdsM"];
 
-    $("#add").click(function(){
-    
-        Song.push($("#song").val());
-    Song.forEach(function(Name){
 
-  $("#songs").append(  Name );
+let songs = [{
+    name: "Hiding",
+    artist: "Florence + The Machine",
+    duration: "2:33",
+    link: "https://youtu.be/d7OoIquEdsM",
+    rating: 4
+}, {
+    name: "Choir",
+    artist: "Florence + The Machine",
+    duration: "2:33",
+    link: "https://www.youtube.com/watch?v=AexrAvyJjJY",
+    rating: 5
+},];
+
+songs.forEach(function (music) {
+    $(".column").append(music.name + music.artist + music.duration + music.link + music.rating);
+})
+
+
+
+let Song = [];
+let Artists = [];
+
+let Duration = [];
+
+let imageAlbum = [];
+
+let Links = [];
+
+$("#add").click(function () {
+
+    Song.push($("#song").val());
+    Song.forEach(function (Name) {
+
+        $("#songs").append(Name);
     });
 
-    
-       Artists.push($("#artist").val());
-    Artists.forEach(function(Singer){
 
-  $("#artists").append( Singer );
+    Artists.push($("#artist").val());
+    Artists.forEach(function (Singer) {
+
+        $("#artists").append(Singer);
     });
 
     Duration.push($("#lengths").val());
-    Duration.forEach(function(length){
+    Duration.forEach(function (length) {
 
-  $("#lengths").append( length );
+        $("#lengths").append(length);
     });
 
-    
+
     imageAlbum.push($("#images").val());
-    imageAlbum.forEach(function(image){
+    imageAlbum.forEach(function (image) {
 
-  $("#images").append("<img src" + image + ">" );
+        $("#images").append("<img src =" + image + ">");
     });
 
-    
+
     Links.push($("#links").val());
-    Links.forEach(function(link){
+    Links.forEach(function (link) {
 
-  $("#links").append("<a href=" + link + ">" + "Link" + "</a>" );
+        $("#links").append("<a href=" + link + ">" + "Link" + "</a>");
     });
-  
 
-    
-    
-    
-    });
+
+
+
+
+});
