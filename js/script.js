7931caf62250e320b8c0549c44d3e5aec6cdc87e
laura <llrstudent03@gmail.com>
2020-02-26 21:51:22 +0000

    some changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-let song1 = {
-    name: "Hiding",
-    artist: "Florence + The Machine",
-    duration: "2:33",
-    link: "https://youtu.be/d7OoIquEdsM"
-};
-let song2 = {
-    name: "Choir",
-    artist: "Florence + The Machine",
-    duration: "2:33",
-    link: "https://www.youtube.com/watch?v=AexrAvyJjJY"
-};
-
-
-
-
-
 
 let songs = [{
     name: "Hiding",
@@ -31,57 +14,16 @@ let songs = [{
 },];
 
 songs.forEach(function (music) {
-    $(".column").append(music.name + music.artist + music.duration + music.link + music.rating);
+    $(".column").append("<li> <div>" + music.name + "</div> <div>" + music.artist +"</div> <div>"+ music.duration + "</div> <div>"+ music.link +"</div> <div>" + music.rating + "</div> </li>");
 })
 
 
 
-let Song = [];
-let Artists = [];
-
-let Duration = [];
-
-let imageAlbum = [];
-
-let Links = [];
-
 $("#add").click(function () {
 
-    Song.push($("#song").val());
-    Song.forEach(function (Name) {
-
-        $("#songs").append(Name);
-    });
-
-
-    Artists.push($("#artist").val());
-    Artists.forEach(function (Singer) {
-
-        $("#artists").append(Singer);
-    });
-
-    Duration.push($("#lengths").val());
-    Duration.forEach(function (length) {
-
-        $("#lengths").append(length);
-    });
-
-
-    imageAlbum.push($("#images").val());
-    imageAlbum.forEach(function (image) {
-
-        $("#images").append("<img src =" + image + ">");
-    });
-
-
-    Links.push($("#links").val());
-    Links.forEach(function (link) {
-
-        $("#links").append("<a href=" + link + ">" + "Link" + "</a>");
-    });
-
-
-
+   songs.forEach(function (music) {
+    $(".column").append("<li>" + music.name + music.artist + music.duration + music.link + music.rating + "</li>");
+})
 
 
 });
